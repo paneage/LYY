47d5347de68d6d5ed60f1249093a55614662516b
paneage <140135263+paneage@users.noreply.github.com>
2023-07-21 15:56:10 +0800

    Update main.py

--- main.py
+++ main.py
@@ -228,6 +228,5 @@ if __name__ == "__main__":
         # 获取词霸每日金句
         note_ch, note_en = get_ciba()
     # 公众号推送消息
-    for user inusers:
         send_message(user, accessToken, region, weather, temp, wind_dir, note_ch, note_en)
     os.system("pause")
