ccf05ff6ee0f80497fef2a453dea7c1462b98456
paneage <140135263+paneage@users.noreply.github.com>
2023-07-21 15:27:47 +0800

    Update main.py

--- main.py
+++ main.py
@@ -93,7 +93,7 @@ if __name__ == "__main__":
     region = config["region"]
     weather, temp, wind_dir = get_weather(region)
     note_ch = config["note_ch"]
-    if note_ch == ":
+    if note_ch :
          # 获取词霸每日金句
          note_ch
     # 公众号推送消息
